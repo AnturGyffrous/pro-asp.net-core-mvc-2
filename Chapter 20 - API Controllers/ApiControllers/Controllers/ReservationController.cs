b4eaeb97e6046258e09d071a326ef360eba7b19b
Owain Williams <git@pilipalasoftware.co.uk>
2018-08-29 18:48:08 +0100

    Add Chapter 20 files created while reading corresponding chapter.

---
+++ Chapter 20 - API Controllers/ApiControllers/Controllers/ReservationController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using ApiControllers.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationController : Controller
+    {
+        private readonly IRepository repository;
+
+        public ReservationController(IRepository repo) => repository = repo;
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id) => repository[id];
+
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    var result = repository[id];
+        //    if (result == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(result);
+        //}
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+            repository.AddReservation(new Reservation
+            {
+                ClientName = res.ClientName,
+                Location = res.Location
+            });
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) => repository.UpdateReservation(res);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<Reservation> patch)
+        {
+            var res = Get(id);
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
+    }
+}
