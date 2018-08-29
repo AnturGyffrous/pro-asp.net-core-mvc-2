4f0b5f05b4181a59baa201116a837be856cccb9c
Owain Williams <git@pilipalasoftware.co.uk>
2018-08-29 18:48:47 +0100

    Add Chapter 16 files created while reading corresponding chapter.

---
+++ Chapter 16 - Advanced Routing Features/UrlsAndRoutes/Infrastructure/LegacyRoute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class LegacyRoute : IRouter
+    {
+        private readonly List<string> _urls;
+
+        private readonly IRouter _mvcRoute;
+
+        public LegacyRoute(IServiceProvider services, params string[] targetUrls)
+        {
+            _urls = new List<string>(targetUrls);
+            _mvcRoute = services.GetRequiredService<MvcRouteHandler>();
+        }
+
+        public async Task RouteAsync(RouteContext context)
+        {
+            var requestedUrl = context.HttpContext.Request.Path.Value.TrimEnd('/');
+            if (_urls.Contains(requestedUrl, StringComparer.OrdinalIgnoreCase))
+            {
+                //context.Handler = async ctx => {
+                //    HttpResponse response = ctx.Response;
+                //    byte[] bytes = Encoding.ASCII.GetBytes($"URL: {requestedUrl}");
+                //    await response.Body.WriteAsync(bytes, 0, bytes.Length);
+                //};
+                context.RouteData.Values["controller"] = "Legacy";
+                context.RouteData.Values["action"] = "GetLegacyUrl";
+                context.RouteData.Values["legacyUrl"] = requestedUrl;
+                await _mvcRoute.RouteAsync(context);
+            }
+            //return Task.CompletedTask;
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            if (context.Values.ContainsKey("legacyUrl"))
+            {
+                var url = context.Values["legacyUrl"] as string;
+                if (_urls.Contains(url))
+                {
+                    return new VirtualPathData(this, url);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Chapter 16 - Advanced Routing Features/UrlsAndRoutes/Infrastructure/WeekDayConstraint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class WeekDayConstraint : IRouteConstraint
+    {
+        private static readonly List<string> Days = new List<string>(new[] {"mon", "tue", "wed", "thu", "fri", "sat", "sun"});
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            return Days.Contains(values[routeKey]?.ToString().ToLowerInvariant());
+        }
+    }
+}
